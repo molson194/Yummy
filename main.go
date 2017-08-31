6a5463f99be52e0be211f1e2b13c5fd5fd3e0429
Matthew Olson <molson194@gmail.com>
2017-08-31 15:57:59 -0600

    Changing port assignment

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 import "net/http"
 import "html/template"
+import "os"
 
 func home(w http.ResponseWriter, r *http.Request) {
 	homePage, _ := template.ParseFiles("templates/home.html")
@@ -13,5 +14,13 @@ func main() {
 	http.HandleFunc("/", home)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	fmt.Println("Starting server...")
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(getPort(), nil)
+}
+
+func getPort() string {
+	var port = os.Getenv("PORT")
+	if port == "" {
+		return ":8080"
+	}
+	return ":" + port
 }
